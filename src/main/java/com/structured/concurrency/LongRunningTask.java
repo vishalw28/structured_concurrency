f640c9f1404a088dabb8a50fcce095cfc88c650d
Vishal Wagh <vishalw28@hotmail.com>
2026-07-22 17:14:43 +0530

    Class which handles long task & return map of response or throw an exception if any one of the tasks fails

--- src/main/java/com/structured/concurrency/LongRunningTask.java
+++ src/main/java/com/structured/concurrency/LongRunningTask.java
@@ -1,6 +1,8 @@
 package com.structured.concurrency;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -10,13 +12,12 @@ import com.structured.concurrency.LongRunningTask.TaskResponse;
 /**
  * LongRunningTask
  */
-public class LongRunningTask implements Callable<TaskResponse>{
+public class LongRunningTask implements Callable<TaskResponse> {
     private final String name;
     private final int time;
     private final String output;
     private final boolean fail;
 
-    
     public LongRunningTask(String name, int time, String output, boolean fail) {
         this.name = name;
         this.time = time;
@@ -24,36 +25,48 @@ public class LongRunningTask implements Callable<TaskResponse>{
         this.fail = fail;
     }
 
-    record TaskResponse(String name, String response, long timeTaken){
+    public String getName(){return name;}
+    record TaskResponse(String name, String response, long timeTaken) {
 
     }
+
     @Override
     public TaskResponse call() throws Exception {
         long start = System.currentTimeMillis();
         print("Started");
         Thread.sleep(Duration.ofSeconds(this.time));
-        if(fail){
+        if (fail) {
             print("Failed");
             throw new RuntimeException(name + " : Failed");
         }
         print("Completed");
         long end = System.currentTimeMillis();
-        return new TaskResponse(name, this.output, end-start);
+        return new TaskResponse(name, this.output, end - start);
     }
 
-
-    private void print(String msg){
+    private void print(String msg) {
         System.out.printf("> %s : %s\n", name, msg);
     }
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("> Main: Started");
         LongRunningTask task = new LongRunningTask("LongTask1", 10, "json-response1", true);
-        try(var service = Executors.newFixedThreadPool(2)){
-            Future<TaskResponse> taskFuture = service.submit(task);
-            Thread.sleep(Duration.ofSeconds(5));
-            taskFuture.cancel(true);
-        }
+        var dbTask = new LongRunningTask("dataTask", 3, "row1", false);
+        var restTask = new LongRunningTask("restTask", 10, "json2", false);
+        var extTask = new LongRunningTask("extTask", 5, "json2", false);
+
+        // execute the sub tasks in parallel.
+        // Throw exception if interrupted or any task fails
+        Map<String, TaskResponse> result = STaskScopeOnFailureExecutor.execute(List.of(dbTask, extTask, restTask));
+
+        // print results of all tasks
+        result.forEach((k, v) -> {
+            System.out.printf("%s : %s\n", k, v);
+        });
+
+        // extract output for an individual task
+        TaskResponse extResponse = result.get("extTask");
+
         System.out.println("> Main: Completed");
     }
 
--- src/main/java/com/structured/concurrency/STaskScopeOnFailureExecutor.java
+++ src/main/java/com/structured/concurrency/STaskScopeOnFailureExecutor.java
@@ -1,11 +1,30 @@
 package com.structured.concurrency;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.StructuredTaskScope.Joiner;
+import java.util.concurrent.StructuredTaskScope.Subtask;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.structured.concurrency.LongRunningTask.TaskResponse;
 
 public class STaskScopeOnFailureExecutor {
-    public static Map<Responses> execute(Collection<LongRunningTask> tasks) throws InterruptedException{
-
-        return null;
+    public static Map<String, TaskResponse> execute(List<LongRunningTask> tasks) throws InterruptedException{
+        Map<String, TaskResponse> map = null;
+        try(var scope = StructuredTaskScope.open(Joiner.awaitAllSuccessfulOrThrow())) {
+            var subTasks = tasks.stream()
+                .map(scope::fork)
+                .toList();
+            scope.join();
+            map = subTasks.stream()
+                .map(t -> t.get())
+                .collect(Collectors.toMap(TaskResponse::name, Function.identity()));
+        }
+        return map;
     }    
 }
